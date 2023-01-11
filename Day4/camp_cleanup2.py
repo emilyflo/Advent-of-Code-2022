8c8c54b113031a6c70da62dfeef1ea3c6b304b55
Emily Starr <emilyflo.starr@gmail.com>
2023-01-11 16:02:31 -0500

    updating work from advent of code

--- Day4/camp_cleanup2.py
+++ Day4/camp_cleanup2.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 import sys
 
-def camp_cleanup(file_name):
+def camp_cleanup2(file_name):
     """
     given two elves' section assignments, compare their sections
     and check if there are any overlapping values.
@@ -17,9 +17,9 @@ def camp_cleanup(file_name):
         section_assignments[pairs] = section_assignments[pairs].replace(',',' ').replace('-',' ').split()
         section_assignments[pairs] = list(map(int,section_assignments[pairs]))
 
-        if section_assignments[pairs][0] <= section_assignments[pairs][2] and section_assignments[pairs][1] >= section_assignments[pairs][3]:
+        if section_assignments[pairs][0] <= section_assignments[pairs][2] and section_assignments[pairs][1] <= section_assignments[pairs][3]:
             overlapping_pairs += 1
-        elif section_assignments[pairs][0] >= section_assignments[pairs][2] and section_assignments[pairs][1] <= section_assignments[pairs][3]: 
+        elif section_assignments[pairs][0] >= section_assignments[pairs][2] and section_assignments[pairs][1] >= section_assignments[pairs][3]: 
             overlapping_pairs += 1
 
     print(overlapping_pairs)
@@ -33,4 +33,4 @@ def read_file(file_name):
         raise TypeError("This is not a file")
 
 if __name__ == "__main__":
-    camp_cleanup(sys.argv[1])
+    camp_cleanup2(sys.argv[1])
--- Day4/tests.py
+++ Day4/tests.py
@@ -1,11 +1,16 @@
 import unittest
-
 from camp_cleanup import *
+from camp_cleanup2 import *
 
 class cleanup(unittest.TestCase):
     def test_cleanup(self):
         answer = camp_cleanup('part1_example.txt')
         self.assertEqual(answer, 2)
+
+class cleanup2(unittest.TestCase):
+    def test_cleanup2(self):
+        answer = camp_cleanup2('part1_example.txt')
+        self.assertEqual(answer, 4)
 
 if __name__ == "__main__":
     unittest.main()
